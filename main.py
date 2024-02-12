e29d8a8006e4281d16d52b6786b3c5e50dfbc683
masondprice <mason.price@students.fhu.edu>
2024-02-12 01:08:39 -0600

    changed return and print

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-#REMOVE PASS AND FIX THIS FUNCTION
+#REMOVE PASS AND FIX THIS FUNCTIONMom
 def palindrome(word):
     lower_word = word.lower()
     reverse_word = lower_word[::-1]
-    if lower_word == reverse_word and word != "":
-        print('True')
+    if lower_word == reverse_word and len(word) != 0:
+        return True
     else:
-        print('False')
+        return False
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
     word = input()
-    palindrome(word)
+    print(palindrome(word))
